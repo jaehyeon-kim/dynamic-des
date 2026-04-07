8715931581443d7f5cc5e244c6f4a269c75e78ba
Jaehyeon Kim <dottami@gmail.com>
2026-04-07 17:23:02 +1000

    bump up to v0.2.0

--- src/dynamic_des/__init__.py
+++ src/dynamic_des/__init__.py
@@ -1,4 +1,5 @@
 import logging
+from importlib.metadata import PackageNotFoundError, version
 
 from dynamic_des.connectors.admin.kafka import KafkaAdminConnector
 from dynamic_des.connectors.egress.kafka import KafkaEgress
@@ -12,9 +13,13 @@ from dynamic_des.models.params import CapacityConfig, DistributionConfig, SimPar
 from dynamic_des.models.schemas import EventPayload, TelemetryPayload
 from dynamic_des.resources.resource import DynamicResource
 
+try:
+    __version__ = version("dynamic-des")
+except PackageNotFoundError:
+    __version__ = "0.0.0-dev"
+
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__version__ = "0.1.0"
 
 __all__ = [
     "DynamicRealtimeEnvironment",
